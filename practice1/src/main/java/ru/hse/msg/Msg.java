3091781bc6ce7d4ab278061b2ad043f05a5dea29
Pavel Solikov <psolikov15@gmail.com>
2019-04-17 14:01:08 +0300

    Added JavaFX app

---
+++ practice1/src/main/java/ru/hse/msg/Msg.java
@@ -0,0 +1,16 @@
+package ru.hse.msg;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Msg extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
