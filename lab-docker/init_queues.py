292695c66f8d57ff75b11d016a4d78dab1a65d17
Mikhail Yutman <myutman@gmail.com>
2019-06-05 23:14:11 +0300

    added increase servicies into docker-compose.yml + added queue getting into init_queues

--- lab-docker/init_queues.py
+++ lab-docker/init_queues.py
@@ -1,13 +1,29 @@
 import boto3
 import time
 
-while True:
-    try:
-        sqs = boto3.resource(service_name="sqs", endpoint_url="http://localstack:4576")
-        sqs.create_queue(QueueName="lol")
-    except:
-        time.sleep(0.228)
-        continue
-    break
-        
-print("halo! ave satan!")
+def create_queue(sqs, name):
+    while True:
+        try:
+            queue = sqs.create_queue(QueueName="lol")
+        except:
+            time.sleep(0.288)
+            continue
+        break
+    return queue
+
+def get_sqs():
+    while True:
+        try:
+            sqs = boto3.resource(service_name="sqs", endpoint_url="http://localstack:4576")
+        except:
+            time.sleep(0.228)
+            continue
+        break
+    return sqs
+
+if __name__ == "__main__":
+    sqs = get_sqs()
+    queueA = create_queue(sqs, "A")
+    queueB = create_queue(sqs, "B")
+    queueA.send_message(MessageBody="1")
+    print("halo! ave satan!")
