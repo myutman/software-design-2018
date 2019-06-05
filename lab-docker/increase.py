caf6d0c2f1d8af45714cf57dc2edf6c9604e93b0
Grigory Bartosh <grishabartosh@yandex.ru>
2019-06-06 00:16:00 +0300

    added flush to print

--- lab-docker/increase.py
+++ lab-docker/increase.py
@@ -18,21 +18,18 @@ def get_queue(sqs, name):
         except:
             time.sleep(1)
 
-print("BBBBBBBBB" * 3)
 if __name__ == "__main__":
-    print("AAAAAAAAAA" * 3)
     queue_a_name = sys.argv[1]
     queue_b_name = sys.argv[2]
 
     sqs = get_sqs()
 
-    print(queue_a_name, queue_b_name)
     queue_a = get_queue(sqs, queue_a_name)
     queue_b = get_queue(sqs, queue_b_name)
 
     while True:
         for message in queue_a.receive_messages():
             x = str(int(message.body) + 1)
-            print(x)
+            print("x =", x, flush=True)
             message.delete()
             queue_b.send_message(MessageBody=x)
