ff8ad27cfa314fe13dbcb44368c00b028f109275
Mikhail Yutman <myutman@gmail.com>
2019-06-05 23:06:13 +0300

    refactoring

--- lab-docker/init_queues.py
+++ lab-docker/init_queues.py
@@ -1,11 +1,13 @@
 import boto3
+import time
 
 while True:
     try:
         sqs = boto3.resource(service_name="sqs", endpoint_url="http://localstack:4576")
         sqs.create_queue(QueueName="lol")
     except:
-        sleep(0.228)
+        time.sleep(0.228)
         continue
+    break
         
 print("halo! ave satan!")
